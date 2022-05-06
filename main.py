54b653a94d24701b13f118d8dc04ef04f288e107
Josué Cortina <joshuacs.main@gmail.com>
2022-05-06 09:17:33 -0400

    Put player level on a variable

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+from telethon import TelegramClient, events
 import os
-import asyncio
 import logging
 import re
 from dotenv import load_dotenv
+
 load_dotenv()
-
-from telethon import TelegramClient, events
-from telethon.events.newmessage import NewMessage
-
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-API_ID = os.getenv('API_ID')
+API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
-
+FIGHTS_ENABLED = True
+FORAY_ENABLED = True
 FIGHT_RE = re.compile(
     r'You met some hostile creatures\. Be careful:\n'
     r'((.|\n)+)'
@@ -28,7 +26,8 @@ SHARE_FIGHT_RE = re.compile(r'https://t\.me/share/url\?url=(/fight_.+)')
 MONSTERS_RE = re.compile(r'(?:(\d) x )?\w+ (\w+) lvl\.(\d+)(\n {2}╰ .+\n)?\n?')
 MONSTERS_TYPES = ['Collector', 'Sentinel', 'Alchemist', 'Ranger', 'Blacksmith', 'Knight', 'Boar', 'Wolf', 'Bear']
 
-GET_FIGHT = True
+LEVEL = 39
+
 
 def send(min_level, amount, monsters):
     m, s = min_level, amount
@@ -39,20 +38,21 @@ def send(min_level, amount, monsters):
     knights = monsters['Knight']
     beasts = bears or wolves or boars
 
-    if m < 34:
+    if m < LEVEL - 10:
         return False
 
     if s == 1:
-        return not bears and m <= 55 or m <= 50
+        return not bears and m <= LEVEL + 15 or m <= LEVEL + 10
 
     if s == 2:
         if beasts:
-            return not bears and m <= 38 or bears == 1 and m <= 36
+            return not bears and m <= LEVEL + 3 or bears == 1 and m <= LEVEL
         else:
-            return not knights and m <= 46 or knights == 1 and m <= 40 or m <= 37
+            return not knights and m <= LEVEL + 10 or knights == 1 and m <= LEVEL or m <= LEVEL - 5
 
     if s == 3:
-        return not beasts and (not knights and m <= 36 or m == 34)
+        return not beasts and (not knights and m <= LEVEL - 5 or m <= LEVEL - 8)
+
 
 def process_monsters(monsters_raw):
     monsters = {m: 0 for m in MONSTERS_TYPES}
@@ -70,19 +70,16 @@ def process_monsters(monsters_raw):
 
     return m, s, monsters
 
+
 with TelegramClient('anon', API_ID, API_HASH) as client:
     @client.on(events.NewMessage(
         incoming=True,
         pattern=FIGHT_RE_BOTATO
     ))
     async def fight_handler_botato(event):
-        global GET_FIGHT
-        
-        if not GET_FIGHT or not event.buttons:
+        if not event.buttons:
             return
             
-        GET_FIGHT = False
-
         monsters_raw = event.pattern_match.group(1)
         m, s, monsters = process_monsters(monsters_raw)
         url = re.match(SHARE_FIGHT_RE, event.buttons[0][0].url).group(1)
@@ -90,30 +87,17 @@ with TelegramClient('anon', API_ID, API_HASH) as client:
         if send(m, s, monsters):
             await client.send_message('chtwrsbot', '%s\n%s' % (monsters_raw, url))
             print('sending fight of %d' % s)
-            # await asyncio.sleep(1000)
-
-        GET_FIGHT = True
 
     @client.on(events.NewMessage(
         incoming=True,
         pattern=FIGHT_RE
     ))
     async def fight_handler(event):
-        global GET_FIGHT
-        
-        if not GET_FIGHT:
-            return
-            
-        GET_FIGHT = False
-
         m, s, monsters = process_monsters(event.pattern_match.group(1))
 
         if send(m, s, monsters):
             await event.forward_to('chtwrsbot')
             print('sending fight of %d' % s)
-            # await asyncio.sleep(1000)
 
-        GET_FIGHT = True
-       
     print('Connected')
     client.run_until_disconnected()
